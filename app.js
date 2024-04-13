3347ae26c8dd4373b22e9d1f3f890931b40c2cbf
Tim <tjm6014@gmail.com>
2024-04-13 10:57:02 -0500

    part 2

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const form = document.querySelector("#add-todo");
 const input = document.querySelector("#name");
 const todoList = document.querySelector("#todo-list");
 
-todoList.addEventListener('click', (e)=> {
-    if (e.target.tagName === "BUTTON") {
-        e.target.parentElement.remove()
-        console.log(currentList)
-    } else if (e.target.tagName === "LI") {
-        e.target.classList.add("crossed-out")
+const savedTodos = JSON.parse(localStorage.getItem("todos"));
+for (let i =0; i<savedTodos.length; i++){
+    const pastTodo = document.createElement("li");
+    pastTodo.innerText = savedTodos[i].task;
+    pastTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+    if (pastTodo.isCompleted) {
+        pastTodo.classList.add("crossed-out")
     }
-})
+    todoList.append(pastTodo)
+}
 
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
     
     const newTodo = document.createElement("li")
     newTodo.innerText = input.value;
+    newTodo.isCompleted = false;
 
     const removeBtn = document.createElement("button")
     removeBtn.innerText = "X";
@@ -23,11 +26,28 @@ form.addEventListener("submit", (e)=> {
     newTodo.append(removeBtn);
     todoList.append(newTodo);
 
-    currentList.task = newTodo.innerText
-    currentList.struck = false;
-    localStorage.setItem("current", JSON.stringify(currentList))
-    console.log(currentList)
-
     input.value = "";
+
+    savedTodos.push({ task: newTodo.innerText, isCompleted: false });
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
 })
 
+
+todoList.addEventListener('click', (e)=> {
+    if (e.target.tagName === "BUTTON") {
+        e.target.parentElement.remove()
+    } else if (e.target.tagName === "LI" && e.target.isCompleted === false) {
+        e.target.classList.add("crossed-out")
+        e.target.isCompleted = true;
+    } else if (e.target.tagName === "LI" && e.target.isCompleted === true) {
+        e.target.classList.remove("crossed-out")
+        e.target.isCompleted = false;
+    }
+
+    for (let i=0; i < savedTodos.length; i++){
+        if (savedTodos[i].task === e.target.innerText) {
+            savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
+            localStorage.setItem("todos", JSON.stringify(savedTodos))
+        }
+    }
+})
