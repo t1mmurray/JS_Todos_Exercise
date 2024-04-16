5a9dc12dea62a26707515e6ed82415e1c737aa32
Tim <tjm6014@gmail.com>
2024-04-16 09:48:24 -0500

    localStorage but fix

--- app.js
+++ app.js
@@ -2,31 +2,41 @@ const form = document.querySelector("#add-todo");
 const input = document.querySelector("#name");
 const todoList = document.querySelector("#todo-list");
 
-// function declaration to be used when removing a specific todo from localStorage
-function removeItemByValue(value){
-    for (let key in localStorage) {
-        if(localStorage.getItem(key) === value) {
-            localStorage.removeItem(key)
-        }
-    }
-}
+
 
 // retrieving past todos
 const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 for (let i = 0; i<savedTodos.length; i++){
+    const newTodoDiv = document.createElement("div");
     const pastTodo = document.createElement("li");
     pastTodo.innerText = savedTodos[i].task;
     pastTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
     if (pastTodo.isCompleted) {
         pastTodo.classList.add("crossed-out")
     }
-    todoList.append(pastTodo)
+    const removeBtn = document.createElement("button")
+    removeBtn.innerText = "X";
+    
+    newTodoDiv.append(pastTodo);
+    newTodoDiv.append(removeBtn);
+    todoList.append(newTodoDiv);
+
 }
 
+// function declaration to be used when removing a specific todo from localStorage
+function removeItemByValue(value){
+    for(let i = 0; i < savedTodos.length; i++){
+        if((savedTodos[i].task) === value){
+          savedTodos.splice(i,1);
+          }
+        }
+        localStorage.setItem('todos', JSON.stringify(savedTodos));
+}
+    
 // adding todos to list and saving to localStorage
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
-    
+    const newTodoDiv = document.createElement("div");
     const newTodo = document.createElement("li")
     newTodo.innerText = input.value;
     newTodo.isCompleted = false;
@@ -34,21 +44,24 @@ form.addEventListener("submit", (e)=> {
     const removeBtn = document.createElement("button")
     removeBtn.innerText = "X";
     
-    newTodo.append(removeBtn);
-    todoList.append(newTodo);
+    newTodoDiv.append(newTodo);
+    newTodoDiv.append(removeBtn);
+    todoList.append(newTodoDiv);
+
+    savedTodos.push({ task: input.value, isCompleted: false });
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
 
     input.value = "";
 
-    savedTodos.push({ task: newTodo.innerText, isCompleted: false });
-    localStorage.setItem("todos", JSON.stringify(savedTodos));
 })
 
 // Delete button, crossing todos off, saving in localStorage
 todoList.addEventListener('click', (e)=> {
     if (e.target.tagName === "BUTTON") {
         e.target.parentElement.remove();
+        console.log(e.target.previousSibling.innerText);
         // This one isn't working! When you click the X button, it should also remove that todo from localStorage
-        removeItemByValue(JSON.stringify(e.target.parentElement.innerText))
+        removeItemByValue(e.target.previousSibling.innerText);
     } else if (e.target.tagName === "LI" && e.target.isCompleted === false) {
         e.target.classList.add("crossed-out")
         e.target.isCompleted = true;
